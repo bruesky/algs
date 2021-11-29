12a8de81aabfe27f12fe3af46c70adfa9657f0ed
Xiangkui Guo <bruce1988cn@gmail.com>
2021-11-29 16:26:11 -0600

    init practice on leetcode

--- src/code/Main.java
+++ src/code/Main.java
@@ -3,9 +3,9 @@ package code;
 import java.util.*;
 
 public class Main {
-    // practice on lintcode
+    // practice on leetcode
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hello lintcode!!!");
+        System.out.println("hello leetcode!!!");
     }
 }
